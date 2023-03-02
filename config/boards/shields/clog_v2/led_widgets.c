63b9257f33e6ecb5e8c6ae615b2dc59793d45ab5
spork <feedthefoon@gmail.com>
2023-03-02 13:59:41 -0800

    missing time param?

--- config/boards/shields/clog_v2/led_widgets.c
+++ config/boards/shields/clog_v2/led_widgets.c
@@ -5,11 +5,4 @@ const led_widget_t led_widgets[LED_EVENT_SIZE][CONFIG_ZMK_LED_WIDGETS_MAX_WIDGET
     [LED_EVENT_BOOT] = {
         WIDGET(0, priority = 100, period = 0, cmd_len = 3, CMD(100, 0, 0, 50), WAIT(100), CMD(100, 0, 0, 50)),
     },
-    [LED_EVENT_LAYER] = {
-        WIDGET(1, priority = 20, period = 0, cmd_len = 1, CMD(0, 20, 12, 0)),
-        WIDGET(2, priority = 20, period = 0, cmd_len = 1, CMD(0, 0, 0, 20)),
-        WIDGET(3, priority = 20, period = 0, cmd_len = 1, CMD(0, 0, 20, 0)),
-        WIDGET(4, priority = 20, period = 0, cmd_len = 1, CMD(0, 10, 37, 34)),
-        WIDGET(5, priority = 20, period = 0, cmd_len = 1, CMD(0, 50, 15, 24)),
-    },
 };
